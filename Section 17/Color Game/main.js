e9d0a3f305a5b85b1aaf16b7e78011843cfe6214
Danny <nanaeacp@gmail.com>
2019-10-10 08:37:58 +0100

    Added selected and hover state for difficulty buttons.

--- Section 17/Color Game/main.js
+++ Section 17/Color Game/main.js
@@ -2,13 +2,25 @@
 let colorToGuess = document.getElementById("colorToGuess");
 let newColors = document.getElementById("newColors");
 let newColorsHover = document.getElementById("newColorsHover");
+let easyDiffHover = document.getElementById("easyDiff");
+let hardDiffHover = document.getElementById("hardDiff");
 
 /* */
 newColors.addEventListener("click", generateAndSetNewColor);
-newColorsHover.addEventListener("hover", toggleHoverColor);
 
+newColorsHover.addEventListener("mouseover", hoverON);
+newColorsHover.addEventListener("mouseout", hoverOFF);
+easyDiffHover.addEventListener("mouseover", hoverON);
+easyDiffHover.addEventListener("mouseout", hoverOFF);
+hardDiffHover.addEventListener("mouseover", hoverON);
+hardDiffHover.addEventListener("mouseout", hoverOFF);
+
+easyDiffHover.addEventListener("click", selectEasyDiff);
+hardDiffHover.addEventListener("click", selectHardDiff);
 /* */
+
 generateAndSetNewColor();
+hardDiffHover.classList.add("selectedColor");
 
 function generateAndSetNewColor()
 {
@@ -19,7 +31,28 @@ function generateAndSetNewColor()
     colorToGuess.innerHTML = `(${colorR}, ${colorG}, ${colorB})`;
 }
 
-function toggleHoverColor()
+function hoverON()
 {
-    newColorsHover.classList.toggle("red");
+    this.classList.add("hoverColor");
+}
+
+function hoverOFF()
+{
+    this.classList.remove("hoverColor");
+}
+
+function selectEasyDiff()
+{
+    easyDiffHover.classList.add("selectedColor");
+    hardDiffHover.classList.remove("selectedColor");
+
+    // TODO: Change game to easy
+}
+
+function selectHardDiff()
+{
+    easyDiffHover.classList.remove("selectedColor");
+    hardDiffHover.classList.add("selectedColor");
+
+    // TODO: Change game to hard
 }
