2a522813f6531a170eccfef9d0664e8be7a7d297
Danny <nanaeacp@gmail.com>
2019-10-16 18:20:16 +0100

    Ctrated delete function with fade. Started adding new entry.

--- Section 20/js/main.js
+++ Section 20/js/main.js
@@ -3,22 +3,58 @@ class App
     constructor()
     {
         this.liItems = document.getElementsByTagName("li");
+        this.spanItems = document.getElementsByTagName("span");
+        this.inputToDo = document.getElementById("inputToDo");
+        this.ulList = document.getElementById("list");
     }
 
     init()
     {
-        for(let i=0; i<this.liItems.length; i++)
+        for (let i = 0; i < this.liItems.length; i++)
         {
-            this.liItems[i].addEventListener("click",this.strikeText);
+            this.liItems[i].addEventListener("click", this.strikeText);
         }
+
+        for (let i = 0; i < this.spanItems.length; i++)
+        {
+            this.spanItems[i].addEventListener("click", this.deleteEntry);
+        }
+
+        this.inputToDo.addEventListener("keyup", this.newEntry);
     }
 
     strikeText()
     {
-        this.classList.contains("todoStrike") ? this.classList.remove("todoStrike") : this.classList.add("todoStrike");
+        this.classList.toggle("todoStrike");
+    }
+
+    deleteEntry(event)
+    {
+        event.stopPropagation();
+        
+        // this == <span> 
+        let liElement = this.parentNode;
+
+        liElement.classList.add("hidden");
+
+        let ulElement = liElement.parentNode;
+        setTimeout(function() { ulElement.removeChild(liElement); }, 550);
+    }
+
+    newEntry(event)
+    {
+        if(event.keyCode === 13)
+        {
+            let entryText = this.value;
+            this.value = "";
+
+            let li = document.createElement("li");
+            li.appendChild(document.createTextNode(entryText));
+            //
+        }
     }
 }
 
-let app = new App(); 
+let app = new App();
 
 app.init();
