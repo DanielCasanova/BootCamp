d321c92f272358cb2e650c0d50eed616654e365a
Danny <nanaeacp@gmail.com>
2019-11-18 08:50:47 +0000

    Added mongoDB.

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 const app = express();
+
+// App port
 const port = 3000;
+
+// MongoDB properties
+// Connection URL
+const url = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'myproject';
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) 
+{
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+   
+    const db = client.db(dbName);
+   
+    client.close();
+});
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }))
